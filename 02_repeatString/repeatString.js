34e582e66b5b884c3b5b8cae7d443988e2686b9f
Hunter <ep.hunter23@gmail.com>
2023-03-11 11:44:40 -0800

    Exercise 1 - 2 done Exercise 3 first pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let hold = ''
 
-};
+    if (num < 0){
+        return 'ERROR'
+    } else {
+        for (i=0; i < num; i++){
+        hold = hold + string
+    }
+       return hold
+}
+}
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
+//holy shit im stupid
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,22 @@
-const reverseString = function() {
+const reverseString = function(splitString) {
+//string into array
+//reverse array
+//array into string
+//return
+  let arraySplit = splitString.split('')
 
+  let newArray= []
+  for (i=splitString.length; i>=0; i--){ 
+    newArray.push(arraySplit[i])
+    if (i === 0) {
+    finalString= newArray.join('')
+    //finalString=finalString.replaceAll(',','') //this was stupid lol using with toString
+    return finalString
+    }
+    
+    //utilize i, find out if you can assign a array to the end of one
+  }
+  
 };
 
 // Do not edit below this line
