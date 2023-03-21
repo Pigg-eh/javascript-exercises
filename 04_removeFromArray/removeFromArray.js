cc2338210676a1918c2f8a5aef2fe0baa49041e4
Hunter <ep.hunter23@gmail.com>
2023-03-21 13:29:42 -0700

    Test 2 passed ex 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(theArray, ...theArgs) {
 
-};
+    for (const arg of theArgs){
+        theArray.splice(theArray.indexOf(arg), 1)
+        
+        if (typeof theArray[indexOf(arg)] === 'undefined'){
+            return theArray  
+        }
+    }
+    return theArray
+}
 
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray
