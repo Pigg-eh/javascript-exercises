81233a0ff13988678068f7dae86b77bbb333aea9
Hunter <ep.hunter23@gmail.com>
2023-03-21 22:02:37 -0700

    Exercise 07 4 tests pass

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  
+  let convert = (tempF -32) * (5/9)
+  let final = convert.toFixed(1)
+  return +final
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let convert = (tempC * (9/5)) + 32
+  let final = convert.toFixed(1)
+  return +final
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
-};
+}; 
