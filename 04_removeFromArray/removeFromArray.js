5ce1a78a55c2616b7b66a839abc5cf68b891008c
Hunter <ep.hunter23@gmail.com>
2023-03-21 20:51:16 -0700

    All tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -4,10 +4,8 @@ const removeFromArray = function(theArray, ...theArgs) {
         if (typeof theArray[theArray.indexOf(arg)] === 'undefined'){
             continue
         } else {
-        theArray.splice(theArray.indexOf(arg), 1)
+            theArray.splice(theArray.indexOf(arg), 1)
         }
-             
-    
     }
     return theArray
 }
