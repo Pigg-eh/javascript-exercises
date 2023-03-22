c359a79062b1967749207e018214371a72751c70
Hunter <ep.hunter23@gmail.com>
2023-03-21 21:15:31 -0700

    Exercise 05 all test pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,7 +2,7 @@ const sumAll = function(a, b) {
 let finalSum 
 let sum = 0
 
-    if (a < 0 || b < 0){
+    if (a < 0 || b < 0 || typeof a != 'number' || typeof b != 'number'){
         return 'ERROR'
     }else if (a < b){
         for (let i=a; i <= b; i++){
