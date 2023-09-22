40de908aa2f236e50ad10448492b43577373bfde
Hunter <ep.hunter23@gmail.com>
2023-09-22 12:38:09 -0700

    Finish calculator
    Factorial here uses recursive mathod

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let ans = 0
+  array.forEach((item)=>{
+    ans += item;
+  });
+  return ans;
 };
 
-const multiply = function() {
 
+const multiply = function(array) {
+  let a = 1;
+    for (i= 0; i<array.length; i++){
+      a *= array[i];
+    }
+return a;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
-};
+function factorial(a) {
+
+  let result = 0
+
+  if(a === 0)result = 1;
+  
+  else result = a * factorial(a-1);
+
+  return result;
+}
 
 // Do not edit below this line
 module.exports = {
