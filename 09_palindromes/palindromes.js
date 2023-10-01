71874df26e0617bf4a592b81f3c4002748a25817
Hunter <ep.hunter23@gmail.com>
2023-10-01 15:30:54 -0700

    finish fibonacci

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let stringGF = string.replace(/\s/g, '')
+    let rawString = stringGF.toLowerCase() .replace(/[^\w\s\']|_/g, '')
+    let reverseString = rawString.split('').reverse().join('')
 
+    console.log(`raw ${rawString}`)
+    console.log(`reverse ${reverseString}`)
+
+    if (rawString === reverseString)  return true;
+    else return false
 };
+
+/*
+learnings:
+    -return defaults to true or false
+    -
+*/
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    
+    n = +n
 
+    if (n < 0){
+        return 'OOPS'  
+    }else if (n === 1|| n === 2) {return 1}
+    
+    else {
+        return fibonacci(n-1) + fibonacci(n-2)
+    }
 };
+
+/* 
+learnings:
+    comparisons need to be written out like above
+    recursive is cool
+    I could have just reassigned the parameter with a unary operator instead of just doing nothing with it 
+*/
 
 // Do not edit below this line
 module.exports = fibonacci;
