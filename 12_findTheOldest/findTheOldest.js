881a6b337704b9c32e2fffda0ef24746ec216118
Hunter <ep.hunter23@gmail.com>
2023-10-07 14:40:02 -0700

    finish exercise 12 (by kinda just coding along)

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,7 +1,21 @@
 const findTheOldest = function(array) {
+    return array.reduce ((oldest, current)=>{
+        const oldestAge = getOldness (oldest.yearOfBirth, oldest.yearOfDeath)
+        const currentAge = getOldness(current.yearOfBirth, current.yearOfDeath)
     
+    return oldestAge < currentAge ? current : oldest;
+    })
+}
+
+
+function getOldness(birth, death) {
+    if(!death) {
+        death = new Date().getFullYear();
+      }
+    return death - birth
+}
     //me confuse
-};
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
